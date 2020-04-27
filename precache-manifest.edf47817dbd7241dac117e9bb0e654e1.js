cb2650e91adced54540ab641a7012294d0b39dfd
a-heard <heardaisha@gmail.com>
2020-04-27 07:47:32 -0400

    Updates

---
+++ precache-manifest.edf47817dbd7241dac117e9bb0e654e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1183ec2d9addbed1b404fed389ecebf1",
+    "url": "/WeatherApp1/index.html"
+  },
+  {
+    "revision": "8f37ce485e61c3c8ee69",
+    "url": "/WeatherApp1/static/css/main.f929c893.chunk.css"
+  },
+  {
+    "revision": "9ceda9c1b0e38959a11b",
+    "url": "/WeatherApp1/static/js/2.37cf47bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp1/static/js/2.37cf47bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f37ce485e61c3c8ee69",
+    "url": "/WeatherApp1/static/js/main.c0d21217.chunk.js"
+  },
+  {
+    "revision": "75c6dee399fb920da7c3",
+    "url": "/WeatherApp1/static/js/runtime-main.9e36ff0d.js"
+  },
+  {
+    "revision": "06f679e2cc3e436715c01681ba2faa77",
+    "url": "/WeatherApp1/static/media/greice-batista-Yw9wawDusx0-unsplash.06f679e2.jpg"
+  }
+]);
